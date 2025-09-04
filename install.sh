a42f8fae9434845b29a4b51384354b6b5a310a9b
Napis <nael82200@outlook.fr>
2025-09-04 20:55:19 +0200

    install.sh test1

--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root or use sudo"
-  exit
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or use sudo"
+  exit 1
 fi
 
-cp -r ./ /usr/bin/theVirtualThings
 
-cat >> /etc/profile.d/thevirtualthings.sh << \EOF
-  PATH=$PATH:/usr/bin/theVirtualThings/tvtcommands
-  PATH=$PATH:/usr/bin/theVirtualThings/UI
-  export PATH
+rm -rf /usr/bin/theVirtualThings
+
+chmod +x /usr/bin/theVirtualThings/tvtcommands/*
+chmod +x /usr/bin/theVirtualThings/UI/*
+sudo cp -r "$(pwd)" /usr/bin/theVirtualThings
+
+
+cat > /etc/profile.d/thevirtualthings.sh << 'EOF'
+PATH=$PATH:/usr/bin/theVirtualThings/tvtcommands
+PATH=$PATH:/usr/bin/theVirtualThings/UI
+export PATH
 EOF
 
-sudo mv ~/.local/share/applications/<application-name.desktop> /usr/share/applications/
+
+cp theVirtualThings.desktop /usr/share/applications/
+
+echo "Installation terminée."
