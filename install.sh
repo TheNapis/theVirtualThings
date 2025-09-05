61e17ad9d238a7e52def5cf2f7042fc54ee420b2
Napis <nael82200@outlook.fr>
2025-09-05 19:48:40 +0200

    install.sh test7

--- install.sh
+++ install.sh
@@ -50,26 +50,26 @@ LOGGEDUSER=$(logname)
 
 echo "Building : arch_mod"
 cd /usr/bin/theVirtualThings/containerImages/archlinuxImages/archlinux
-su $LOGGEDUSER -c 'podman build -t arch_mod .'
+su - $LOGGEDUSER -c 'podman build -t arch_mod .'
 
 echo "Building : arch_xfce"
 cd /usr/bin/theVirtualThings/containerImages/archlinuxImages/archlinuxxfce
-su $LOGGEDUSER -c 'podman build -t arch_xfce .'
+su - $LOGGEDUSER -c 'podman build -t arch_xfce .'
 
 echo "Building : deb_mod"
 cd /usr/bin/theVirtualThings/containerImages/debianImages/debian
-su $LOGGEDUSER -c 'podman build -t deb_mod .'
+su - $LOGGEDUSER -c 'podman build -t deb_mod .'
 
 echo "Building : debian_xfce"
 cd /usr/bin/theVirtualThings/containerImages/debianImages/debianxfce
-su $LOGGEDUSER -c 'podman build -t debian_xfce .'
+su - $LOGGEDUSER -c 'podman build -t debian_xfce .'
 
 echo "Building : fedora_mod"
 cd /usr/bin/theVirtualThings/containerImages/fedoraImages/fedora
-su $LOGGEDUSER -c 'podman build -t fedora_mod .'
+su -  $LOGGEDUSER -c 'podman build -t fedora_mod .'
 
 echo "Building : fedora_xfce"
 cd /usr/bin/theVirtualThings/containerImages/fedoraImages/fedoraxfce
-su $LOGGEDUSER -c 'podman build -t fedora_xfce .'
+su - $LOGGEDUSER -c 'podman build -t fedora_xfce .'
 
 echo "Installation terminée."
